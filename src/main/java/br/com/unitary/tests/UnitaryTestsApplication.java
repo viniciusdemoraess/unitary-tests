cbd8348333acc2682b1939a96fea8e493d3e0c57
root <root@localhost.localdomain>
2023-05-08 14:29:42 -0400

    First commit

---
+++ src/main/java/br/com/unitary/tests/UnitaryTestsApplication.java
@@ -0,0 +1,13 @@
+package br.com.unitary.tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitaryTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitaryTestsApplication.class, args);
+	}
+
+}
